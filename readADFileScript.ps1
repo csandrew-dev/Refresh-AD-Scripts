e7388946d561a23b29e0e75b8de2af6f80384576
monty hall <montyhall@mail.com>
2023-06-28 14:10:34 -0500

    added try catch block for execution safety

--- readADFileScript.ps1
+++ readADFileScript.ps1
@@ -7,8 +7,10 @@ $VERSION = 06282023 #started this file, moves file to local and changes to ps1 f
 $INFILE_PATH = "\\Test-DC\umd-its-share\ComputersToUMD-REFRESH.txt" #path to generated file
 $EXCFILE_PATH = "C:\ComputersToUMD-REFRESH.ps1" #new path to run file as powershell on local machine cause can't run script without signage.
 
-Move-Item -Path $INFILE_PATH -Destination $EXCFILE_PATH #moves the file and changes to .ps1 for executing
-
-& $EXCFILE_PATH #executes file 
-
-Remove-Item -Path $EXCFILE_PATH #removes file so no errors occur during the move next time.
+try{
+    Move-Item -Path $INFILE_PATH -Destination $EXCFILE_PATH -ErrorAction Stop #moves the file and changes to .ps1 for executing
+    & $EXCFILE_PATH #executes file 
+    Remove-Item -Path $EXCFILE_PATH #removes file so no errors occur during the move next time.
+}catch [System.Management.Automation.ItemNotFoundException]{ #catch file not found error, means no machines needed to be added to refresh
+    Write-Host "File does not exist, not machines to image."
+}
